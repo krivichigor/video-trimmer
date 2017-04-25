451a64ad696ea0f74a58b3a19a097e81aa172a5b
Igor <krivich.igor@gmail.com>
2017-04-25 11:51:27 +0300

    Switching between MySQL & MongoDB for User model

--- app/User.php
+++ app/User.php
@@ -2,17 +2,27 @@
 
 namespace App;
 
+use Illuminate\Auth\Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
+use Illuminate\Foundation\Auth\Access\Authorizable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
-class User extends Authenticatable
+
+class User extends Eloquent implements AuthenticatableContract, AuthorizableContract
 {
-    use Notifiable;
+    use Notifiable, Authenticatable, Authorizable;
 
     protected $primaryKey = 'api_token';
 
     public $incrementing = false;
 
+    protected $collection = 'users_collection';
+
+    protected $connection = 'mongodb';
     /**
      * The attributes that are mass assignable.
      *
